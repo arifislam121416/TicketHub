16c9c05d170fc5e6dc90169ff4b7fc34e5ecb2a2
arifislam121416 <ardairyhelthcare@gmail.com>
2026-07-30 11:31:02 +0600

    vercel live link add

--- src/app/lib/auth.js
+++ src/app/lib/auth.js
@@ -14,7 +14,7 @@ export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
 
   trustedOrigins: [
-    "http://localhost:3000",
+    process.env.BETTER_AUTH_URL,
   ],
 
   database: mongodbAdapter(db, {
